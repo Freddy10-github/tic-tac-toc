0f9871423537f36105b2e5818ccc3df94172a508
Freddy <a0983330053@gmail.com>
2021-08-05 14:13:14 +0800

    fix juge fail

--- index.js
+++ index.js
@@ -47,19 +47,3 @@ gameBoxs.forEach(gameBox=>{
 })
 
 
-
-// const judgeWin =()=>{
-//     let jud1 = ($('#box1').text())==($('#box2').text())&&($('#box2').text())==($('#box3').text());
-//     let jud2 = ($('#box4').text())==($('#box5').text())&&($('#box5').text())==($('#box6').text());
-//     let jud3 = ($('#box7').text())==($('#box8').text())&&($('#box8').text())==($('#box9').text());
-//     let jud4 = ($('#box1').text())==($('#box4').text())&&($('#box4').text())==($('#box7').text());
-//     let jud5 = ($('#box2').text())==($('#box5').text())&&($('#box5').text())==($('#box8').text());
-//     let jud6 = ($('#box3').text())==($('#box6').text())&&($('#box6').text())==($('#box9').text());
-//     let jud7 = ($('#box1').text())==($('#box5').text())&&($('#box5').text())==($('#box9').text());
-//     let jud8 = ($('#box3').text())==($('#box5').text())&&($('#box5').text())==($('#box7').text());
-
-//     if(jud1||jud2||jud3||jud4||jud5||jud6||jud7||jud8){
-//         return alert("Game Over\n"+gamer[round%2]+" Win ");
-//         gameIsOver = 1 ;
-//     }
-// }
